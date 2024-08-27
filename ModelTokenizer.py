89e55160e7a1b347183a36c87c1027f6522249e7
Snehadhole <50446219+Snehadhole@users.noreply.github.com>
2024-08-27 16:36:15 +0530

    Update ModelTokenizer.py

--- ModelTokenizer.py
+++ ModelTokenizer.py
@@ -1,3 +1,5 @@
+from transformers import AutoModelForSeq2SeqLM, AutoTokenize
+
 def get_model_tokenizer(model_name = "google/flan-t5-base"):
   original_model = AutoModelForSeq2SeqLM.from_pretrained(model_name, torch_dtype=torch.float32)
   tokenizer = AutoTokenizer.from_pretrained(model_name)
