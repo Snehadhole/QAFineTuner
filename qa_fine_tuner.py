c0106318739f1d94e5ac9a9bf175aa52576a3e6b
Snehadhole <50446219+Snehadhole@users.noreply.github.com>
2024-08-27 17:37:34 +0530

    Update qa_fine_tuner.py

--- qa_fine_tuner.py
+++ qa_fine_tuner.py
@@ -1,24 +1,24 @@
-import opendatasets as od
 import pandas as pd
 from datasets import Dataset
 import datasets
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig, TrainingArguments, Trainer
-import torch
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer , GenerationConfig, TrainingArguments, Trainer
 import time
-import re
+import torch
 from CleanText import clean_text
-from ModelTokenizer import get_model_tokenizer
 from TokenizeFunction import tokenize_function
+import json
 # Detect the GPU if any; if not, use CPU. If on a Mac with M1/M2, consider using MPS.
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+with open('config.json', 'r') as file:
+  config = json.load(file)
 
 # Load the dataset into a Pandas DataFrame, selecting only the 'question' and 'answer' columns
 data = pd.read_csv("NvidiaDocumentationQandApairs.csv")[["question", "answer"]]
 
 # Print the shape of the dataset and display the first few rows
-print(data.shape)
-print(data.head())
+# print(data.shape)
+# print(data.head())
 
 data['question'] = data['question'].apply(clean_text)
 data['answer'] = data['answer'].apply(clean_text)
@@ -29,7 +29,12 @@ test=data.drop(train.index) # Create testing by removing the 70% of the train da
 val=test.sample(frac=0.5,random_state=7) # Create validation of 50% of the testing data
 test=test.drop(val.index) # Create testing by removing the 50% of the validation data which will result in 50%
 
-model, tokenizer = get_model_tokenizer()
+
+
+model_name = config['model_name']
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name, torch_dtype=torch.float32)
+
+# model = get_model_tokenizer()
 
 train_data = Dataset.from_pandas(train)
 train_tokenized_datasets = train_data.map(tokenize_function, batched=True)
@@ -45,11 +50,11 @@ test_data = Dataset.from_pandas(test)
 test_tokenized_datasets = test_data.map(tokenize_function, batched=True)
 test_tokenized_datasets = test_tokenized_datasets.remove_columns(['question', 'answer','__index_level_0__'])
 
-EPOCHS = 1
-LR = 1e-4
-BATCH_SIZE = 2
+EPOCHS = config["EPOCHS"] #1
+LR = config["LR"] #1e-4
+BATCH_SIZE = config["BATCH_SIZE"] #2
 
-training_path = "./training_path_nvidia_chatbot"
+training_path = config["training_path"]#"./training_path_nvidia_chatbot"
 
 
 training_args = TrainingArguments(
@@ -73,6 +78,6 @@ trainer= Trainer(
 
 trainer.train()
 
-model_path = "./nvidia-chatbot-final-model"
+model_path = config["model_path"]#"./nvidia-chatbot-final-model"
 
 trainer.model.save_pretrained(model_path)
