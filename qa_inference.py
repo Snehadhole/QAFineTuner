6835e865bf6bab7731a0513a2d6611309bfaf6bb
Snehadhole <50446219+Snehadhole@users.noreply.github.com>
2024-08-27 20:39:39 +0530

    Create qa_inference.py

---
+++ qa_inference.py
@@ -0,0 +1,9 @@
+def ModelInference(test_text,model_path) :
+    trained_model, tokenizer = get_model_tokenizer(model_path)
+
+    tokenized_test_text = tokenizer(test_text,
+                                  return_tensors='pt')
+    model_output = trained_model.generate(tokenized_test_text.input_ids,
+                                          generation_config=GenerationConfig(max_new_tokens=200, num_beams=1),)[0]
+    final_output = tokenizer.decode(model_output, skip_special_tokens=True)
+    return final_output
